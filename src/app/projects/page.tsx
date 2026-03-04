657ded842035f33c9eb9542b928415eb6540d15a
markasif <apasif243@gmail.com>
2026-03-04 16:45:32 +0530

    Done

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { projectsData } from "@/data/projectsData";
 import Image from "next/image";
 import Link from "next/link";
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { expertiseData } from "@/data/homeData";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
